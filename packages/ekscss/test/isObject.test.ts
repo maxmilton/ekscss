f17e50cfbeab0541fc4554b03cd9d758c4e8fb68
Max Milton <max@wearegenki.com>
2021-05-11 11:16:56 +1000

    Add isObject helper tests

--- packages/ekscss/test/isObject.test.ts
+++ packages/ekscss/test/isObject.test.ts
@@ -1,52 +1,71 @@
-/* eslint-disable max-len */
+/* eslint-disable import/no-extraneous-dependencies */
 
-// class Foo{}
+import { test } from 'uvu';
+import * as assert from 'uvu/assert';
+import * as allExports from '../src/helpers';
+import { isObject } from '../src/helpers';
 
-// https://github.com/jonschlinkert/isobject/blob/master/test.js
+function Func() {}
+class Cls {}
 
-// it('should be true when the value is an object.', function() {
-//   assert(isObject({}));
-//   assert(isObject(Object.create({})));
-//   assert(isObject(Object.create(Object.prototype)));
-//   assert(isObject(Object.create(null)));
-//   assert(isObject(/foo/)); // <<< WTF?
+const objects = [
+  [{}, '{}'],
+  [{ a: 1, b: true }, '{}'],
+  [Object.create({}), 'Object.create({})'],
+  [Object.create(Object.prototype), 'Object.create(Object.prototype)'],
+  [Object.create(null), 'Object.create(null)'],
+  [new Func(), 'new Func'],
+  [new Func(), 'new Func()'],
+  [new Cls(), 'new Cls'],
+  [new Cls(), 'new Cls()'],
+] as const;
 
-//   function Foo() {}
-//   assert(isObject(new Foo));
-//   assert(isObject(new Foo()));
-// });
+const notObjects = [
+  ['abc', "'abc'"],
+  ['', "''"],
+  [1, '1'],
+  [0, '0'],
+  [-1, '-1'],
+  [Infinity, 'Infinity'],
+  [-Infinity, '-Infinity'],
+  [BigInt(Number.MAX_SAFE_INTEGER), 'BigInt(Number.MAX_SAFE_INTEGER)'],
+  [true, 'true'],
+  [false, 'false'],
+  [[], '[]'],
+  [['abc', 'def'], "['abc', 'def']"],
+  [function named() {}, 'function named() {}'],
+  // eslint-disable-next-line func-names
+  [function () {}, 'function () {}'],
+  [() => {}, '() => {}'],
+  [async () => {}, 'async () => {}'],
+  [/regex/, '/regex/'],
+  [Object, 'Object'],
+  [Symbol, 'Symbol'],
+  [Symbol.toStringTag, 'Symbol.toStringTag'],
+  [Symbol.prototype, 'Symbol.prototype'],
+  [undefined, 'undefined'],
+  [null, 'null'],
+  // eslint-disable-next-line no-sparse-arrays
+  [, '<no value>'],
+  [global, 'global'],
+  [performance, 'performance'],
+] as const;
 
-// it('should be false when the value is not an object.', function() {
-//   assert(!isObject('whatever'));
-//   assert(!isObject(1));
-//   assert(!isObject(function() {}));
-//   assert(!isObject([]));
-//   assert(!isObject(['foo', 'bar']));
-//   assert(!isObject());
-//   assert(!isObject(undefined));
-//   assert(!isObject(null));
-// });
+test('exports an "isObject" function', () => {
+  assert.is('isObject' in allExports, true);
+  assert.type(allExports.isObject, 'function');
+});
 
-// https://github.com/jonschlinkert/is-plain-object/blob/master/test/server.js
+objects.forEach(([value, name]) => {
+  test(`returns true for "${name}" object`, () => {
+    assert.is(isObject(value), true);
+  });
+});
 
-// it('should return `true` if the object is created by the `Object` constructor.', function() {
-//   assert(isPlainObject(Object.create({})));
-//   assert(isPlainObject(Object.create(Object.prototype)));
-//   assert(isPlainObject({foo: 'bar'}));
-//   assert(isPlainObject({}));
-//   assert(isPlainObject(Object.create(null)));
-// });
+notObjects.forEach(([value, name]) => {
+  test(`returns false for "${name}" non-object`, () => {
+    assert.is(isObject(value), false);
+  });
+});
 
-// it('should return `false` if the object is not created by the `Object` constructor.', function() {
-//   function Foo() {this.abc = {};};
-
-//   assert(!isPlainObject(/foo/));
-//   assert(!isPlainObject(function() {}));
-//   assert(!isPlainObject(1));
-//   assert(!isPlainObject(['foo', 'bar']));
-//   assert(!isPlainObject([]));
-//   assert(!isPlainObject(new Foo));
-//   assert(!isPlainObject(null));
-// });
-
-export {};
+test.run();
