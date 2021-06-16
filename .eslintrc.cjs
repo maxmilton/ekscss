94b973743a3807b1518112227754e39732025030
Max Milton <max@wearegenki.com>
2021-06-16 13:58:53 +1000

    Set up eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,4 +1,5 @@
 const OFF = 0;
+const WARN = 1;
 
 // eslint-disable-next-line max-len
 /** @type {import('eslint').Linter.Config & { parserOptions: import('@typescript-eslint/types').ParserOptions }} */
@@ -16,8 +17,18 @@ module.exports = {
     'airbnb-typescript/base',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:unicorn/recommended',
   ],
   rules: {
     'import/prefer-default-export': OFF,
+    'unicorn/filename-case': OFF,
+    'unicorn/no-abusive-eslint-disable': WARN,
+    'unicorn/no-null': OFF,
+    'unicorn/prefer-add-event-listener': OFF,
+    'unicorn/prefer-dom-node-append': OFF,
+    'unicorn/prefer-module': OFF,
+    'unicorn/prefer-node-protocol': OFF,
+    'unicorn/prefer-query-selector': OFF,
+    'unicorn/prevent-abbreviations': OFF,
   },
 };
