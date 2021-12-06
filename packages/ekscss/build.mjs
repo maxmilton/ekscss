b8ffebbe0e6efeafd4a60bd72825903d6b3ede85
Max Milton <max@happycode.ltd>
2021-12-06 16:12:04 +1000

    Use iife in builds instead of top-level await for old node support

--- packages/ekscss/build.mjs
+++ packages/ekscss/build.mjs
@@ -5,64 +5,66 @@ import esbuild from 'esbuild';
 const mode = process.env.NODE_ENV;
 const dev = mode === 'development';
 
-// Standard node CJS bundle
-const out1 = await esbuild.build({
-  entryPoints: ['src/index.ts'],
-  outfile: 'dist/index.js',
-  platform: 'node',
-  target: ['node10'],
-  define: {
-    'process.env.BROWSER': 'false',
-    'process.env.NODE_ENV': JSON.stringify(mode),
-  },
-  external: ['source-map', 'stylis'],
-  banner: { js: '"use strict";' },
-  bundle: true,
-  sourcemap: true,
-  minify: !dev,
-  watch: dev,
-  metafile: !dev && process.stdout.isTTY,
-  logLevel: 'debug',
-});
-
-if (out1.metafile) {
-  console.log(await esbuild.analyzeMetafile(out1.metafile));
-}
-
-// Browser compatible ESM bundle (without sourcemap support)
-const out2 = await esbuild.build({
-  entryPoints: ['src/index.ts'],
-  outfile: 'dist/browser.mjs',
-  platform: 'browser',
-  format: 'esm',
-  define: {
-    __filename: JSON.stringify(''),
-    'process.env.BROWSER': 'true',
-    'process.env.NODE_ENV': JSON.stringify(mode),
-  },
-  external: ['stylis'],
-  bundle: true,
-  sourcemap: true,
-  minifySyntax: !dev,
-  watch: dev,
-  metafile: !dev && process.stdout.isTTY,
-  logLevel: 'debug',
-  plugins: [
-    {
-      name: 'mock-sourcemap',
-      setup(build) {
-        build.onResolve({ filter: /^\.\/sourcemap$/ }, () => ({
-          namespace: 'mock-sourcemap',
-          path: 'null',
-        }));
-        build.onLoad({ filter: /.*/, namespace: 'mock-sourcemap' }, () => ({
-          contents: 'export const compileSourceMap = () => {};',
-        }));
-      },
+(async () => {
+  // Standard node CJS bundle
+  const out1 = await esbuild.build({
+    entryPoints: ['src/index.ts'],
+    outfile: 'dist/index.js',
+    platform: 'node',
+    target: ['node10'],
+    define: {
+      'process.env.BROWSER': 'false',
+      'process.env.NODE_ENV': JSON.stringify(mode),
     },
-  ],
-});
+    external: ['source-map', 'stylis'],
+    banner: { js: '"use strict";' },
+    bundle: true,
+    sourcemap: true,
+    minify: !dev,
+    watch: dev,
+    metafile: !dev && process.stdout.isTTY,
+    logLevel: 'debug',
+  });
 
-if (out2.metafile) {
-  console.log(await esbuild.analyzeMetafile(out2.metafile));
-}
+  if (out1.metafile) {
+    console.log(await esbuild.analyzeMetafile(out1.metafile));
+  }
+
+  // Browser compatible ESM bundle (without sourcemap support)
+  const out2 = await esbuild.build({
+    entryPoints: ['src/index.ts'],
+    outfile: 'dist/browser.mjs',
+    platform: 'browser',
+    format: 'esm',
+    define: {
+      __filename: JSON.stringify(''),
+      'process.env.BROWSER': 'true',
+      'process.env.NODE_ENV': JSON.stringify(mode),
+    },
+    external: ['stylis'],
+    bundle: true,
+    sourcemap: true,
+    minifySyntax: !dev,
+    watch: dev,
+    metafile: !dev && process.stdout.isTTY,
+    logLevel: 'debug',
+    plugins: [
+      {
+        name: 'mock-sourcemap',
+        setup(build) {
+          build.onResolve({ filter: /^\.\/sourcemap$/ }, () => ({
+            namespace: 'mock-sourcemap',
+            path: 'null',
+          }));
+          build.onLoad({ filter: /.*/, namespace: 'mock-sourcemap' }, () => ({
+            contents: 'export const compileSourceMap = () => {};',
+          }));
+        },
+      },
+    ],
+  });
+
+  if (out2.metafile) {
+    console.log(await esbuild.analyzeMetafile(out2.metafile));
+  }
+})().catch(console.error);
--- packages/postcss-ekscss/build.mjs
+++ packages/postcss-ekscss/build.mjs
@@ -5,21 +5,23 @@ import esbuild from 'esbuild';
 const mode = process.env.NODE_ENV;
 const dev = mode === 'development';
 
-const out = await esbuild.build({
-  entryPoints: ['src/index.ts'],
-  outfile: 'dist/index.js',
-  platform: 'node',
-  target: ['node12'],
-  external: ['postcss'],
-  banner: { js: '"use strict";' },
-  bundle: true,
-  sourcemap: true,
-  minifySyntax: !dev,
-  watch: dev,
-  metafile: !dev && process.stdout.isTTY,
-  logLevel: 'debug',
-});
+(async () => {
+  const out = await esbuild.build({
+    entryPoints: ['src/index.ts'],
+    outfile: 'dist/index.js',
+    platform: 'node',
+    target: ['node12'],
+    external: ['postcss'],
+    banner: { js: '"use strict";' },
+    bundle: true,
+    sourcemap: true,
+    minifySyntax: !dev,
+    watch: dev,
+    metafile: !dev && process.stdout.isTTY,
+    logLevel: 'debug',
+  });
 
-if (out.metafile) {
-  console.log(await esbuild.analyzeMetafile(out.metafile));
-}
+  if (out.metafile) {
+    console.log(await esbuild.analyzeMetafile(out.metafile));
+  }
+})().catch(console.error);
