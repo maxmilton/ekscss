c8f980ebebb29a23f66340e85ff8714b2aebc36d
Max Milton <max@happycode.ltd>
2022-07-06 20:47:40 +1000

    chore: Fix new lint errors

--- packages/postcss-ekscss/build.mjs
+++ packages/postcss-ekscss/build.mjs
@@ -5,22 +5,20 @@ import esbuild from 'esbuild';
 const mode = process.env.NODE_ENV;
 const dev = mode === 'development';
 
-(async () => {
-  const out = await esbuild.build({
-    entryPoints: ['src/index.ts'],
-    outfile: 'dist/index.js',
-    platform: 'node',
-    target: ['node12'],
-    external: ['postcss'],
-    bundle: true,
-    sourcemap: true,
-    minifySyntax: !dev,
-    watch: dev,
-    metafile: !dev && process.stdout.isTTY,
-    logLevel: 'debug',
-  });
+const out = await esbuild.build({
+  entryPoints: ['src/index.ts'],
+  outfile: 'dist/index.js',
+  platform: 'node',
+  target: ['node12'],
+  external: ['postcss'],
+  bundle: true,
+  sourcemap: true,
+  minifySyntax: !dev,
+  watch: dev,
+  metafile: !dev && process.stdout.isTTY,
+  logLevel: 'debug',
+});
 
-  if (out.metafile) {
-    console.log(await esbuild.analyzeMetafile(out.metafile));
-  }
-})().catch(console.error);
+if (out.metafile) {
+  console.log(await esbuild.analyzeMetafile(out.metafile));
+}
