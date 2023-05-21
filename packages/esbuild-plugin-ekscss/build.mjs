3291e8ef069c722395e413202698617696ff5832
Max Milton <max@happyhackers.xyz>
2023-05-21 14:10:34 +1000

    feat: Add es module files to packages + build packages via `esbuild` rather than `tsc` (#655)

---
+++ packages/esbuild-plugin-ekscss/build.mjs
@@ -0,0 +1,62 @@
+/* eslint-disable unicorn/prefer-top-level-await, unicorn/no-process-exit, no-console */
+
+import esbuild from 'esbuild';
+
+const mode = process.env.NODE_ENV || 'production';
+const dev = mode === 'development';
+
+(async () => {
+  // Node CJS bundle
+  /** @type {esbuild.BuildOptions} */
+  const esbuildConfig1 = {
+    entryPoints: ['src/index.ts'],
+    outfile: 'dist/index.js',
+    platform: 'node',
+    target: ['node12'],
+    define: {
+      'process.env.NODE_ENV': JSON.stringify(mode),
+    },
+    external: ['ekscss', 'stylis'],
+    bundle: true,
+    sourcemap: true,
+    minify: !dev,
+    metafile: !dev && process.stdout.isTTY,
+    logLevel: 'debug',
+  };
+
+  // Node ESM bundle
+  /** @type {esbuild.BuildOptions} */
+  const esbuildConfig2 = {
+    entryPoints: ['src/index.ts'],
+    outfile: 'dist/index.mjs',
+    platform: 'node',
+    format: 'esm',
+    target: ['node16'],
+    define: {
+      'process.env.NODE_ENV': JSON.stringify(mode),
+    },
+    external: ['ekscss', 'stylis'],
+    bundle: true,
+    sourcemap: true,
+    minify: !dev,
+    metafile: !dev && process.stdout.isTTY,
+    logLevel: 'debug',
+  };
+
+  if (dev) {
+    const context1 = await esbuild.context(esbuildConfig1);
+    const context2 = await esbuild.context(esbuildConfig2);
+    await Promise.all([context1.watch(), context2.watch()]);
+  } else {
+    const out1 = await esbuild.build(esbuildConfig1);
+    const out2 = await esbuild.build(esbuildConfig2);
+
+    if (out1.metafile)
+      console.log(await esbuild.analyzeMetafile(out1.metafile));
+    if (out2.metafile)
+      console.log(await esbuild.analyzeMetafile(out2.metafile));
+  }
+})().catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
