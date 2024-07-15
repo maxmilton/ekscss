1a6ed7a7ac6e18b0ea1d79eb1ef309edd1ebf7d9
Max Milton <max@happyhackers.xyz>
2024-07-15 19:52:08 +0900

    feat(postcss-ekscss): Manually declare types

---
+++ packages/postcss-ekscss/types.d.ts
@@ -0,0 +1,7 @@
+import type { Parser, Stringifier } from 'postcss';
+
+export declare const parse: Parser;
+export declare const stringify: Stringifier;
+
+declare const main: { parse; stringify };
+export default main;
