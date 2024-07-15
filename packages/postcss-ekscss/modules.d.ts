622f04d3470ac0cab934624dce2cefdf3ed06034
Max Milton <max@happyhackers.xyz>
2024-07-15 19:52:35 +0900

    bug(postcss-ekscss): Fix external types

--- packages/postcss-ekscss/modules.d.ts
+++ packages/postcss-ekscss/modules.d.ts
@@ -1,9 +1,9 @@
 declare module 'postcss/lib/parser' {
   import type { Input, Node, Root } from 'postcss';
-  import type { Token, Tokenizer } from './src/tokenizer';
+  import type { Token, Tokenizer } from 'postcss-ekscss/src/tokenize';
 
-  // https://github.com/postcss/postcss/blob/main/lib/parser.js#L10
-  declare class Parser {
+  // https://github.com/postcss/postcss/blob/e0efb168c8a65b42ba10b787857cfa306ba9d48c/lib/parser.js#L23
+  class Parser {
     tokenizer: Tokenizer;
 
     input: Input & { error: (message: string, pos?: number) => void };
@@ -33,7 +33,7 @@ declare module 'postcss/lib/parser' {
     spacesFromEnd(tokens: Token[]): string;
     stringFrom(tokens: Token[], from: number): string;
     precheckMissedSemicolon(tokens?: Token[]): void;
-    checkMissedSemicolon(tokens: Tokens[]): void;
+    checkMissedSemicolon(tokens: Token[]): void;
   }
 
   export default Parser;
