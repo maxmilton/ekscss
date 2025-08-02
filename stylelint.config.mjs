66810f453b464acf448bc6930aeb2ace1471cd8b
Max Milton <max@happyhackers.xyz>
2025-08-02 12:44:03 +0900

    chore: Update StyleLint config

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,26 +1,29 @@
-/** @type {import('stylelint').Config} */
+/** @type {import("stylelint").Config} */
 export default {
   reportInvalidScopeDisables: true,
   reportNeedlessDisables: true,
-  extends: ['stylelint-config-standard', '@maxmilton/stylelint-config'],
+  extends: [
+    "stylelint-config-standard",
+    "@maxmilton/stylelint-config",
+    "@maxmilton/stylelint-config/xcss",
+  ],
   ignoreFiles: [
-    '**/*.bak/**',
-    '**/dist/**',
-    '**/node_modules/**',
+    "**/*.bak/**",
+    "**/dist/**",
+    "**/node_modules/**",
 
     // FIXME: Remove these when fixed.
-    'packages/framework/addon/alert.xcss',
-    'packages/framework/addon/ux-larger-click-zone.xcss',
-    'packages/framework/level1/type.xcss',
-    'packages/framework/level2/display.xcss',
-    'packages/framework/level2/flex.xcss',
-    'packages/framework/level2/form.xcss',
-    'packages/framework/level2/grid.xcss',
-    'packages/framework/level2/position.xcss',
-    'packages/framework/level2/spacing.xcss',
+    "packages/framework/addon/alert.xcss",
+    "packages/framework/addon/ux-larger-click-zone.xcss",
+    "packages/framework/level1/type.xcss",
+    "packages/framework/level2/display.xcss",
+    "packages/framework/level2/flex.xcss",
+    "packages/framework/level2/form.xcss",
+    "packages/framework/level2/grid.xcss",
+    "packages/framework/level2/position.xcss",
+    "packages/framework/level2/spacing.xcss",
   ],
   rules: {
-    'import-notation': null,
-    'media-query-no-invalid': null,
+    "media-query-no-invalid": null,
   },
 };
