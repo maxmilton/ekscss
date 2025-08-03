93bc524e460e25dbffa83f75e8aa85c7e3fe5296
Max Milton <max@happyhackers.xyz>
2025-08-03 20:37:45 +0900

    feat: Add new config-loader package
    To replace `joycon` package dependency

---
+++ packages/config-loader/build.ts
@@ -0,0 +1,62 @@
+import { createTypes } from "@ekscss/build-tools";
+import esbuild, { type BuildOptions } from "esbuild";
+
+const mode = process.env.NODE_ENV;
+const dev = mode === "development";
+
+console.time("prebuild");
+await Bun.$`rm -rf dist`;
+console.timeEnd("prebuild");
+
+// Node CJS bundle
+const esbuildConfig1: BuildOptions = {
+  entryPoints: ["src/index.ts"],
+  outfile: "dist/index.js",
+  platform: "node",
+  target: ["node12"],
+  define: {
+    "process.env.NODE_ENV": JSON.stringify(mode),
+  },
+  external: [],
+  bundle: true,
+  sourcemap: true,
+  minify: !dev,
+  metafile: !dev && process.stdout.isTTY,
+  logLevel: "debug",
+};
+
+// Node ESM bundle
+const esbuildConfig2: BuildOptions = {
+  entryPoints: ["src/index.ts"],
+  outfile: "dist/index.mjs",
+  platform: "node",
+  format: "esm",
+  target: ["node16"],
+  define: {
+    "process.env.NODE_ENV": JSON.stringify(mode),
+  },
+  external: [],
+  bundle: true,
+  sourcemap: true,
+  minify: !dev,
+  metafile: !dev && process.stdout.isTTY,
+  logLevel: "debug",
+};
+
+console.time("dts");
+createTypes(["src/index.ts"], "dist");
+console.timeEnd("dts");
+
+if (dev) {
+  const context1 = await esbuild.context(esbuildConfig1);
+  const context2 = await esbuild.context(esbuildConfig2);
+  await Promise.all([context1.watch(), context2.watch()]);
+} else {
+  console.time("build");
+  const out1 = await esbuild.build(esbuildConfig1);
+  const out2 = await esbuild.build(esbuildConfig2);
+  console.timeEnd("build");
+
+  if (out1.metafile) console.log(await esbuild.analyzeMetafile(out1.metafile));
+  if (out2.metafile) console.log(await esbuild.analyzeMetafile(out2.metafile));
+}
---
+++ packages/config-loader/src/index.ts
@@ -0,0 +1,137 @@
+// Based on https://github.com/egoist/joycon/blob/4c1e87691bfe0b3b5ffc46f5ac546ed4c710a9d2/src/index.js (MIT)
+
+/* eslint-disable @typescript-eslint/no-require-imports, @typescript-eslint/prefer-nullish-coalescing, global-require */
+
+import fs from "fs";
+import path from "path";
+
+const pathExists = (filePath: string): Promise<boolean> =>
+  new Promise((resolve) => {
+    fs.access(filePath, (error) => {
+      resolve(!error);
+    });
+  });
+
+interface ConfigLoaderOptions {
+  files: string[];
+  cwd?: string;
+  stopDir?: string;
+  packageKey?: string;
+  noCache?: boolean;
+}
+
+interface ConfigLoaderResult {
+  path: string;
+  data: unknown;
+}
+
+export class ConfigLoader {
+  files: string[];
+  cwd: string;
+  stopDir: string;
+  packageKey: string | undefined;
+  noCache: boolean;
+
+  loadCache = new Map<string, unknown>();
+  existsCache = new Map<string, boolean>();
+  packageJsonCache = new Map<string, unknown>();
+
+  constructor({
+    files,
+    cwd = process.cwd(),
+    stopDir,
+    packageKey,
+    noCache = process.env.NODE_ENV !== "test",
+  }: ConfigLoaderOptions) {
+    // eslint-disable-next-line unicorn/explicit-length-check
+    if (!files.length) {
+      throw new TypeError("files must be a non-empty array");
+    }
+
+    this.files = files;
+    this.cwd = path.resolve(cwd);
+    this.stopDir = stopDir
+      ? path.resolve(stopDir)
+      : path.parse(this.cwd).root;
+    this.packageKey = packageKey;
+    this.noCache = noCache;
+  }
+
+  clearCache(): void {
+    this.loadCache.clear();
+    this.existsCache.clear();
+    this.packageJsonCache.clear();
+  }
+
+  async resolve(dir: string): Promise<string | null> {
+    // Don't traverse above the module root
+    if (dir === this.stopDir || path.basename(dir) === "node_modules") {
+      return null;
+    }
+
+    for (const fileName of this.files) {
+      const filePath = path.resolve(dir, fileName);
+      let exists = this.existsCache.get(filePath);
+
+      if (exists === undefined) {
+        // eslint-disable-next-line no-await-in-loop
+        exists = await pathExists(filePath);
+        if (!this.noCache) {
+          this.existsCache.set(filePath, exists);
+        }
+      }
+
+      // eslint-disable-next-line no-continue
+      if (!exists) continue;
+
+      if (!this.packageKey || path.basename(filePath) !== "package.json") {
+        return filePath;
+      }
+
+      // eslint-disable-next-line @typescript-eslint/no-dynamic-delete
+      delete require.cache[filePath];
+      const pkg = require(filePath) as Record<string, unknown>;
+
+      if (Object.prototype.hasOwnProperty.call(pkg, this.packageKey)) {
+        this.packageJsonCache.set(filePath, pkg[this.packageKey]);
+        return filePath;
+      }
+    }
+
+    // Continue in parent directory
+    return this.resolve(path.dirname(dir));
+  }
+
+  async load(configPath?: string): Promise<ConfigLoaderResult | null> {
+    const filePath = configPath
+      ? path.resolve(configPath)
+      : await this.resolve(this.cwd);
+
+    if (!filePath) return null;
+
+    let data = this.loadCache.get(filePath);
+    if (!data) {
+      data = await this.loadData(filePath);
+      this.loadCache.set(filePath, data);
+    }
+
+    return { path: filePath, data };
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-redundant-type-constituents
+  private loadData(filePath: string): unknown | Promise<unknown> {
+    const extname = path.extname(filePath);
+    if (extname === ".js" || extname === ".ts" || extname === ".mjs" || extname === ".cjs") {
+      // eslint-disable-next-line @typescript-eslint/no-dynamic-delete
+      delete require.cache[filePath];
+      const mod = require(filePath) as Record<string, unknown>;
+      return mod.default || mod;
+    }
+
+    if (this.packageJsonCache.has(filePath)) {
+      return this.packageJsonCache.get(filePath);
+    }
+
+    return JSON.parse(fs.readFileSync(filePath, "utf8"));
+  }
+}
