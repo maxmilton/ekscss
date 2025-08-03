9935a6139066ca6589c53b57bb8550101f353c1c
Max Milton <max@happyhackers.xyz>
2025-08-03 20:39:46 +0900

    feat(framework): Refactor

--- packages/framework/xcss.config.mjs
+++ packages/framework/xcss.config.mjs
@@ -3,21 +3,22 @@
  * config into your own project, you should use `config.js` not this file.
  */
 
-"use strict";
+import { ctx, onBeforeBuild } from "ekscss";
+import { readFileSync } from "node:fs";
+import { basename } from "node:path";
+import { fileURLToPath } from "node:url";
+import baseConfig from "./config.mjs";
+import { preloadApply } from "./utils.mjs";
 
-const { ctx, onBeforeBuild } = require("ekscss");
-const path = require("path");
-const pkg = require("./package.json");
-const baseConfig = require("./config.js");
-const { preloadApply } = require("./utils.js");
+/** @type {import("./package.json")} */
+const pkg = JSON.parse(readFileSync(fileURLToPath(new URL("package.json", import.meta.url)), "utf8"));
 
 /** @type {string} */
 let bundleName;
 
 onBeforeBuild(() => {
   if (ctx.from) {
-    bundleName = (ctx.from.includes("/addon/") ? "addon/" : "")
-      + path.basename(ctx.from, ".xcss");
+    bundleName = (ctx.from.includes("/addon/") ? "addon/" : "") + basename(ctx.from, ".xcss");
 
     // pre-populate applyPlugin ctx.applyRefs for #apply in addon/native.xcss
     if (ctx.from.endsWith("framework/addon/native.xcss")) {
@@ -32,9 +33,9 @@ onBeforeBuild(() => {
  * Note: This config **should not be reused** in other projects. What you likely
  * want is `@ekscss/framework/config`.
  *
- * @type {import('@ekscss/cli').XCSSConfig}
+ * @type {import("@ekscss/cli").Config}
  */
-module.exports = {
+export default {
   ...baseConfig,
   get banner() {
     return `/*!
